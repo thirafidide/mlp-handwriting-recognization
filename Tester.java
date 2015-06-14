a9410b8a9ee50d000623a5b2b5fbf250fb2a882d
al <rifqialfatih@gmail.com>
2015-06-14 14:46:03 +0700

    edit Tester.java

--- Tester.java
+++ Tester.java
@@ -2,51 +2,107 @@ import java.util.ArrayList;
 
 public class Tester {
 
-	public static final int NEURON_INPUT = 784;
-	public static final int NEURON_HIDDEN = 784 * 20;
-	public static final int NEURON_OUTPUT = 10;
-	public static final int NUMBER_OF_TEST = 10000;
+	public static final int NUMBER_OF_TEST = 10;
+	public static final double TOL = 0.01;
+	public static final int ITERATION_LIMIT = 500;
+
+
+	public static final double LEARNING_RATE = 0.1;
+	public static final int N_HIDDEN_LAYER = 2 * 675 + 1;
 
 	public static void main(String[] args) {
 
-		MLP mlp = new MLP(NEURON_INPUT, NEURON_HIDDEN, NEURON_OUTPUT);
-		mlp.setLearningRate(0.7);
-
 		try {
-			CSVReader cr = new CSVReader();
-			ArrayList<double[]> dataset = cr.read("dataset/d5problem-original.dataset.csv", NUMBER_OF_TEST);
+			ArrayList[] dataset = MLPinput.read("dataset/d5problem-original.dataset.csv");
+			ArrayList<ArrayList> tempPattern = dataset[1];
+			ArrayList<Integer> tempOutput = dataset[0];
+			double[][] normalizedData = new double[tempPattern.size()][tempPattern.get(0).size()];
+			double[][] normalizedDesiredOutput = new double[tempOutput.size()][10];
 
 
-			for (int i=0;i<NUMBER_OF_TEST;i++) {
-				double[] datai = dataset.get(i);
-				double[] pattern = new double[datai.length-10];
-				double[] desiredOutput = new double[10];
+			for (int i=0;i<tempPattern.size();i++) {
 
-				for (int j=0;j<pattern.length;j++) {
-					pattern[j] = datai[j];
+				ArrayList<Integer> pattern = tempPattern.get(i);
+				int output = tempOutput.get(i);
+
+				normalizedDesiredOutput[i][output] = 1;
+				for (int j=0;j<pattern.size();j++) {
+					normalizedData[i][j] = pattern.get(j);
 				}
-
-				for (int j=0;j<10;j++) {
-					desiredOutput[j] = datai[datai.length-10+j];
-				}
-				//System.out.println(pattern.length + " " + desiredOutput.length);
-
-				double[] output = mlp.train(pattern, desiredOutput);
-
-				double err = 0;
-				for (int j=1;j<output.length;j++) { 
-					//System.out.print(output[j] + " "); 
-					err += Math.abs(output[j] - desiredOutput[j-1]);
-				}
-				//System.out.println();
-				//for (int j=0;j<desiredOutput.length;j++) System.out.print(desiredOutput[j] + " "); System.out.println();
-				System.out.println(err);
-
 			}
+			
+			crossValidation(normalizedData, normalizedDesiredOutput, NUMBER_OF_TEST);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void crossValidation(double[][] patterns, double[][] desiredOutput, int numberOfTest) {
+
+		int numberOfDataEachTest = patterns.length / numberOfTest;
+
+		for (int i=0;i<numberOfTest;i++) {
+
+			MLP mlp = new MLP(patterns[0].length, N_HIDDEN_LAYER, desiredOutput[0].length);
+			mlp.setLearningRate(LEARNING_RATE);
+
+
+			// training
+			train(mlp, patterns, desiredOutput, numberOfDataEachTest, i, true);
+
+			// validation
+			double validationError = 0;
+			for (int j=numberOfDataEachTest*i;j<numberOfDataEachTest*i+numberOfDataEachTest;j++) {
+				double[] output = mlp.passNet(patterns[j]);
+
+				validationError += evaluateError(output, desiredOutput[j]);
+			}
+			validationError /= numberOfDataEachTest;
+			System.out.println("Validation error: " + validationError);
+
+		}
+	}
+
+	public static void train(MLP mlp, double[][] patterns, double[][] desiredOutput, int numberOfDataEachTest, int iteration, boolean isUsingIteration) {
+		if (isUsingIteration) {
+			for (int i=0;i<ITERATION_LIMIT;i++) {
+				double err = 0;
+				for (int j=0;j<patterns.length;j++) {
+					if (j/numberOfDataEachTest == iteration) continue;
+					
+					double[] output = mlp.train(patterns[j], desiredOutput[j]);
+
+					//System.out.println(output[1] + " " + desiredOutput[j][0]);
+					err += evaluateError(output, desiredOutput[j]);
+				}
+				err /= patterns.length;
+				System.out.println(err);
+			}
+		}
+		else {
+			double err = Double.POSITIVE_INFINITY;
+			while (err > TOL) {
+				err = 0;
+				for (int j=0;j<patterns.length;j++) {
+					if (j/numberOfDataEachTest == iteration) continue;
+					
+					double[] output = mlp.train(patterns[j], desiredOutput[j]);
+					err += evaluateError(output, desiredOutput[j]);
+				}
+				err /= patterns.length;
+				System.out.println(err);
+			}
+		}
+	}
+
+	public static double evaluateError(double[] output, double[] desiredOutput) {
+		double res = 0;
+		for (int i=0;i<desiredOutput.length;i++) {
+			res += (output[i+1] - desiredOutput[i]) * (output[i+1] - desiredOutput[i]);
+		}
+
+		return Math.sqrt(res);
 	}
 
 
