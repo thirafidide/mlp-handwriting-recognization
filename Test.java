6493642221e9321bae0271c863cf6c4108ccd682
pandhu <pandhu.hutomo@ui.ac.id>
2015-06-14 11:36:24 +0700

    input preprocessing

---
+++ Test.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+class Test{	
+	public static void main( String[] args) {
+      ArrayList[] output = MLPinput.read(args[0]);
+      MLPinput.printData(output[1]);
+    }
+}
