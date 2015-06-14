73921f142a14d3982ba6a13e6bf38d2035762f43
pandhu <pandhu.hutomo@ui.ac.id>
2015-06-14 13:46:42 +0700

    banyak kolom

--- MLPinput.java
+++ MLPinput.java
@@ -33,8 +33,8 @@ public class MLPinput{
                 }
                 arrData.add(arrInnerData);
             }
+            //System.out.println("Kolom Awal:" +arrData.get(0).size());
             arrData = preprocessing(arrData);       
-    
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
