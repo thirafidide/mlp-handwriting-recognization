9de710883b66151731e30610e8e3dd015467600d
al <rifqialfatih@gmail.com>
2015-06-14 14:50:46 +0700

    delete Test.java

--- Test.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-class Test{	
-	public static void main( String[] args) {
-      ArrayList[] output = MLPinput.read(args[0]);
-      MLPinput.printData(output[1]);
-    }
-}
